5f3f19ece15881710b0f7023334d115d0c2ca2fc
dilipRathoreRepo <rathore.gecg@gmail.com>
2019-11-01 16:57:33 +0530

    adding files

---
+++ constants.py
@@ -0,0 +1,8 @@
+import oauth2
+
+CONSUMER_KEY = 'X2TFs1C4CjLSw4DRBHTMXRPQH'
+CONSUMER_SECRET = 'eX8s3Bv4BS5NFrKi5Q92mqXEDqtL2f4ypnnU1KrjOd4ZEY9355'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
---
+++ dummy.py
@@ -0,0 +1,10 @@
+# import psycopg2
+from user import User
+from database import Database
+
+Database.initialise(user='postgres', password='admin', database='learning', host='localhost')
+try:
+    user = User.load_from_db_by_email('rathore.gec@gmail.com')
+    print('oauth_token and oauth_token_secret are : {} and {}'.format(user.oauth_token, user.oauth_token_secret))
+except TypeError:
+    print("invalid email")
