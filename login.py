6cbd20a6f562813499f6686d378dff0082e42d57
dilipRathoreRepo <rathore.gecg@gmail.com>
2019-11-01 19:29:06 +0530

    moving new files in feature branch

--- login.py
+++ login.py
@@ -1,30 +1,20 @@
 import constants
 import oauth2
 import urllib.parse as urlparse
-import json
 from user import User
 from database import Database
+from twitter_utils import consumer
 
 email = 'rathore.gecg@gmail.com'
+URI = 'https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images'
+VERB = 'GET'
 
-# Consumer is just used to identify the app, it cannot be used to make API requests
-consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
-
-# Client is used for making API requests
 client = oauth2.Client(consumer)
 
-# Check if entry already exists for user, if yes skip fetching new PIN
-# Initialize database connection pool
 Database.initialise(user='postgres', password='admin', database='learning', host='localhost')
+user = User.load_from_db_by_email(email)
 
-try:
-    user = User.load_from_db_by_email(email)
-    print('Already existing oauth_token and oauth_token_secret are : {} and {}'.format(
-        user.oauth_token, user.oauth_token_secret))
-    oauth_token = user.oauth_token
-    oauth_token_secret = user.oauth_token_secret
-
-except TypeError:
+if not user:
     print("Entry does not exist.. creating a record in users table")
 
     response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
@@ -32,10 +22,9 @@ except TypeError:
     if response.status != 200:
         print("An error occurred getting request token from Twitter!")
 
-    # qsl = query string parameter. It is used to parse the content
     request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
-    print("Go to the following site in your brower:")
+    print("Go to the following site in your browser:")
     print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
 
     oauth_verifier = input("what is the PIN?")
@@ -47,26 +36,13 @@ except TypeError:
     response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
 
     access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
-    print(access_token)
 
-    oauth_token = access_token['oauth_token']
-    oauth_token_secret = access_token['oauth_token_secret']
+    first_name = input("Enter user first name")
+    last_name = input("Enter user last name")
+    user = User(email, first_name, last_name, None, access_token['oauth_token'], access_token['oauth_token_secret'])
+    user.save_to_db()
 
-    # Save user details
-    dilip = User(email, 'Dilip', 'Rathore', None, oauth_token, oauth_token_secret)
-    dilip.save_to_db()
-
-# Create an authorized token object and use that to perform twitter API calls on behalf of the user
-authorized_token = oauth2.Token(oauth_token, oauth_token_secret)
-authorized_client = oauth2.Client(consumer, authorized_token)
-
-# Make Twitter API calls
-response, content = authorized_client.request('https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images', 'GET')
-if response.status != 200:
-    print('An error occurred while searching!')
-
-print(content.decode('utf-8'))
-tweets = json.loads(content.decode('utf-8'))
+content = user.twitter_request(URI)
+tweets = content
 
 [print(tweet["text"]) for tweet in tweets["statuses"]]
-
--- user.py
+++ user.py
@@ -1,4 +1,9 @@
+import json
+
+import oauth2
+
 from database import CursorFromConnectionFromPool
+from twitter_utils import consumer
 
 
 class User:
@@ -26,3 +31,13 @@ class User:
             user_data = cursor.fetchone()
             return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0],
                        oauth_token=user_data[4], oauth_token_secret=user_data[5])
+
+    def twitter_request(self, uri, verb='GET'):
+        authorized_token = oauth2.Token(self.oauth_token, self.oauth_token_secret)
+        authorized_client = oauth2.Client(consumer, authorized_token)
+
+        response, content = authorized_client.request(uri, verb)
+        if response.status != 200:
+            print('An error occurred while searching!')
+        else:
+            return json.loads(content.decode('utf-8'))
