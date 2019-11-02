9e7d981be4b458eb489ff96a632a4d93dc7b3f8d
dilipRathoreRepo <rathore.gecg@gmail.com>
2019-11-02 13:37:40 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 
-CONSUMER_KEY = 'X2TFs1C4CjLSw4DRBHTMXRPQH'
-CONSUMER_SECRET = 'eX8s3Bv4BS5NFrKi5Q92mqXEDqtL2f4ypnnU1KrjOd4ZEY9355'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
