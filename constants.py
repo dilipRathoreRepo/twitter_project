93ae476e4479a8c373a341d6ed9bf17b9f6d41e0
dilipRathoreRepo <rathore.gecg@gmail.com>
2019-11-03 19:37:01 +0530

    tokenizing

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 
-CONSUMER_KEY = 'M7veJ6ip5iTA3eEmPXQx8u0CJ'
-CONSUMER_SECRET = 'XO3whqmtKMflfESWTylGDPs2fGXBToi40DHyqOJ2zTZf0Qz77m'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
